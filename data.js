73f9dd5312adcab4992bac1985521c613f7d7e35
jmluceno <jmluceno@gmail.com>
2017-07-28 13:29:19 -0700

    data file
    Data file contains the main JSON object

---
+++ data.js
@@ -0,0 +1,820 @@
+var data = {
+				start: [
+					
+					{
+						scale: "King",
+						c: "#FFF",
+						colorName: ""
+					},
+					{
+						scale: "Queen",
+						c: "#FFF",
+						colorName: ""
+					},
+					{
+						scale: "Emperor",
+						c: "#FFF",
+						colorName: ""
+					},
+					{
+						scale: "Empress",
+						c: "#FFF",
+						colorName: ""
+					}
+				],
+				spherePrimum: [
+					
+					{
+						scale: "King",
+						c: "#FFF",
+						colorName: "Brilliance"
+					},
+					{
+						scale: "Queen",
+						c: "#FFF",
+						colorName: "White brilliance"
+					},
+					{
+						scale: "Emperor",
+						c: "#FFF",
+						colorName: "White brilliance"
+					},
+					{
+						scale: "Empress",
+						c: "#FFF",
+						flecked: ["#FEDD00"],
+						colorName: "White flecked gold"
+					}
+				],
+				sphereFixed: [
+					{
+						scale: "King",
+						c: "#8ABAD3",
+						colorName: "Pure soft blue"
+					},
+					{
+						scale: "Queen",
+						c: "#808080",
+						colorName: "Grey"
+					},
+					{
+						scale: "Emperor",
+						c: "#a4b2bf",
+						colorName: "Blue pearl grey, like mother-of pearl"
+					},
+					{
+						scale: "Empress",
+						c: "#FFF",
+						colorName: "White, flecked red, blue, and yellow",
+						flecked: ["#ed1c24", "#0085ca", "#FEDD00"]
+					}
+				],
+				sphereSaturn: [
+					{
+						scale: "King",
+						c: "#AE0E36",
+						colorName: "Crimson"
+					},
+					{
+						scale: "Queen",
+						c: "#000000",
+						colorName: "Black"
+					},
+					{
+						scale: "Emperor",
+						c: "#38200a",
+						colorName: "Dark brown"
+					},
+					{
+						scale: "Empress",
+						c: "#808080",
+						flecked: ["#FDC3C6"],
+						colorName: "Grey flecked pink"
+					}
+				],
+				sphereDaath: [
+					{
+						scale: "King",
+						c: "#9A90CD",
+						colorName: "Lavender"
+					},
+					{
+						scale: "Queen",
+						c: "#bfbfbf",
+						colorName: "Grey-white"
+					},
+					{
+						scale: "Emperor",
+						c: "#5b00cc",
+						colorName: "Pure Violet"
+					},
+					{
+						scale: "Empress",
+						c: "#808080",
+						flecked: ["#FEDD00"],
+						colorName: "Grey flecked gold"
+					}
+				],
+				sphereJupiter: [
+					{
+						scale: "King",
+						c: "#2d0066",
+						colorName: "Deep Violet"
+					},
+					{
+						scale: "Queen",
+						c: "#0085ca",
+						colorName: "Blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#550055",
+						colorName: "Deep purple"
+					},
+					{
+						scale: "Empress",
+						c: "#1f649a",
+						flecked: ["#FEDD00"],
+						colorName: "Deep azure, flecked yellow"
+					}
+				],
+				sphereMars: [
+					{
+						scale: "King",
+						c: "#FF6D00",
+						colorName: "Orange"
+					},
+					{
+						scale: "Queen",
+						c: "#F2301B",
+						colorName: "Scarlet red"
+					},
+					{
+						scale: "Emperor",
+						c: "#ff321c",
+						colorName: "Bright scarlet"
+					},
+					{
+						scale: "Empress",
+						c: "#ed1c24",
+						flecked: ["#000000"],
+						colorName: "Red, flecked black"
+					}
+				],
+				sphereSun: [
+					{
+						scale: "King",
+						c: "#FDC3C6",
+						colorName: "Clear pink rose"
+					},
+					{
+						scale: "Queen",
+						c: "#FEDD00",
+						colorName: "Yellow (gold)"
+					},
+					{
+						scale: "Emperor",
+						c: "#f25330",
+						colorName: "Rich salmon"
+					},
+					{
+						scale: "Empress",
+						c: "#FFCA1A",
+						colorName: "Gold amber"
+					}
+				],
+				sphereVenus: [
+					{
+						scale: "King",
+						c: "#ffb734",
+						colorName: "Amber"
+					},
+					{
+						scale: "Queen",
+						c: "#00A550",
+						colorName: "Emerald"
+					},
+					{
+						scale: "Emperor",
+						c: "#e0f317",
+						colorName: "Bright yellow green"
+					},
+					{
+						scale: "Empress",
+						c: "#8d8800",
+						flecked: ["#FEDD00"],
+						colorName: "Olive, flecked gold"
+					}
+				],
+				sphereMercury: [
+					{
+						scale: "King",
+						c: "#7f14aa",
+						colorName: "Violet Purple"
+					},
+					{
+						scale: "Queen",
+						c: "#FF6D00",
+						colorName: "Orange"
+					},
+					{
+						scale: "Emperor",
+						c: "#B1282B",
+						colorName: "Red-russet"
+					},
+					{
+						scale: "Empress",
+						c: "#AD8809",
+						flecked: ["#ffffff"],
+						colorName: "Yellow-brown, flecked white"
+					}
+				],
+				sphereLuna: [
+					{
+						scale: "King",
+						c: "#001489",
+						colorName: "Indigo"
+					},
+					{
+						scale: "Queen",
+						c: "#440099",
+						colorName: "Violet"
+					},
+					{
+						scale: "Emperor",
+						c: "#330b33",
+						colorName: "Very dark purple"
+					},
+					{
+						scale: "Empress",
+						c: "#dedb2c",
+						flecked: ["#4D91C6"],
+						colorName: "Citrine, flecked azure"
+					}
+				],
+				sphereElements: [
+					{
+						scale: "King",
+						c: "#FEDD00",
+						colorName: "Yellow"
+					},
+					{
+						scale: "Queen",
+						quartered: ["#dedb2c", "#8d8800", "#731817", "#000000"],
+						colorName: "Citrine, olive, russet, and black"
+					},
+					{
+						scale: "Emperor",
+						quartered: ["#dedb2c", "#8d8800", "#731817", "#000000"], 
+						flecked: ["#FEDD00"],
+						colorName: "As Queen scale, but flecked with gold"
+					},
+					{
+						scale: "Empress",
+						c: "#000000",
+						rayed: "#FEDD00",
+						colorName: "Black rayed with yellow"
+					}
+				],
+				air: [
+					{
+						scale: "King",
+						c: "#fee74d",
+						colorName: "Bright pale yellow"
+					},
+					{
+						scale: "Queen",
+						c: "#8ABAD3",
+						colorName: "Sky blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#00958D",
+						colorName: "Blue emerald green"
+					},
+					{
+						scale: "Empress",
+						c: "#00A550",
+						flecked: ["#FFB000"],
+						colorName: "Emerald, flecked gold"
+					}
+				],
+				mercury: [
+					{
+						scale: "King",
+						c: "#FEDD00",
+						colorName: "Yellow"
+					},
+					{
+						scale: "Queen",
+						c: "#BB29BB",
+						colorName: "Purple"
+					},
+					{
+						scale: "Emperor",
+						c: "#808080",
+						colorName: "Grey"
+					},
+					{
+						scale: "Empress",
+						c: "#001489",
+						rayed: "#440099",
+						colorName: "Indigo, rayed violet"
+					}
+				],
+				luna: [
+					{
+						scale: "King",
+						c: "#0085ca",
+						colorName: "Blue"
+					},
+					{
+						scale: "Queen",
+						c: "#e8e8e8",
+						colorName: "Silver"
+					},
+					{
+						scale: "Emperor",
+						c: "#A5C5D9",
+						colorName: "Cold pale blue"
+					},
+					{
+						scale: "Empress",
+						c: "#e8e8e8",
+						rayed: "#8ABAD3",
+						colorName: "Silver, rayed sky blue"
+					}
+				],
+				venus: [
+					{
+						scale: "King",
+						c: "#00A550",
+						colorName: "Emerald Green"
+					},
+					{
+						scale: "Queen",
+						c: "#8ABAD3",
+						colorName: "Sky blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#b2d135",
+						colorName: "Early spring green"
+					},
+					{
+						scale: "Empress",
+						c: "#a41247",
+						rayed: "#B2E79F",
+						colorName: "Bright rose or cerise, rayed pale green"
+					}
+				],
+				aries: [
+					{
+						scale: "King",
+						c: "#F2301B",
+						colorName: "Scarlet"
+					},
+					{
+						scale: "Queen",
+						c: "#ed1c24",
+						colorName: "Red"
+					},
+					{
+						scale: "Emperor",
+						c: "#f23200",
+						colorName: "Brilliant flame"
+					},
+					{
+						scale: "Empress",
+						c: "#ff1500",
+						colorName: "Glowing red"
+					}
+				],
+				taurus: [
+					{
+						scale: "King",
+						c: "#FF4E00",
+						colorName: "Red orange"
+					},
+					{
+						scale: "Queen",
+						c: "#000834",
+						colorName: "Deep indigo"
+					},
+					{
+						scale: "Emperor",
+						c: "#C7B63C",
+						colorName: "Deep warm olive"
+					},
+					{
+						scale: "Empress",
+						c: "#5c3312",
+						colorName: "Rich brown"
+					}
+				],
+				gemini: [
+					{
+						scale: "King",
+						c: "#FF6D00",
+						colorName: "Orange"
+					},
+					{
+						scale: "Queen",
+						c: "#cca3b1",
+						colorName: "Pale Mauve"
+					},
+					{
+						scale: "Emperor",
+						c: "#e8af36",
+						colorName: "New yellow leather"
+					},
+					{
+						scale: "Empress",
+						c: "#907172",
+						colorName: "Reddish grey inclined to mauve"
+					}
+				],
+				cancer: [
+					{
+						scale: "King",
+						c: "#ffb734",
+						colorName: "Amber"
+					},
+					{
+						scale: "Queen",
+						c: "#800f13",
+						colorName: "Maroon"
+					},
+					{
+						scale: "Emperor",
+						c: "#d60d0a",
+						colorName: "Rich bright russet"
+					},
+					{
+						scale: "Empress",
+						c: "#322F12",
+						colorName: "Dark greenish brown"
+					}
+				],
+				leo: [
+					{
+						scale: "King",
+						c: "#E5D708",
+						colorName: "Yellow, greenish"
+					},
+					{
+						scale: "Queen",
+						c: "#550055",
+						colorName: "Deep purple"
+					},
+					{
+						scale: "Emperor",
+						c: "#808080",
+						colorName: "Grey"
+					},
+					{
+						scale: "Empress",
+						c: "#ff9934",
+						colorName: "Reddish amber"
+					}
+				],
+				virgo: [
+					{
+						scale: "King",
+						c: "#59B934",
+						colorName: "Green, yellowish"
+					},
+					{
+						scale: "Queen",
+						c: "#9faeaa",
+						colorName: "Slate grey"
+					},
+					{
+						scale: "Emperor",
+						c: "#558c70",
+						colorName: "Green grey"
+					},
+					{
+						scale: "Empress",
+						c: "#981733",
+						colorName: "Plum colour"
+					}
+				],
+				jupiter: [
+					{
+						scale: "King",
+						c: "#7f14aa",
+						colorName: "Violet Purple"
+					},
+					{
+						scale: "Queen",
+						c: "#0085ca",
+						colorName: "Blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#990099",
+						colorName: "Rich purple"
+					},
+					{
+						scale: "Empress",
+						c: "#00a8ff",
+						rayed: "#fedd00",
+						colorName: "Bright blue, rayed yellow"
+					}
+				],
+				libra: [
+					{
+						scale: "King",
+						c: "#00A550",
+						colorName: "Emerald Green"
+					},
+					{
+						scale: "Queen",
+						c: "#0085ca",
+						colorName: "Blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#008077",
+						colorName: "Deep blue-green"
+					},
+					{
+						scale: "Empress",
+						c: "#B2E79F",
+						colorName: "Pale green"
+					}
+				],
+				water: [
+					{
+						scale: "King",
+						c: "#004dab",
+						colorName: "Deep blue"
+					},
+					{
+						scale: "Queen",
+						c: "#149C88",
+						colorName: "Sea green"
+					},
+					{
+						scale: "Emperor",
+						c: "#5b6300",
+						colorName: "Deep olive-green"
+					},
+					{
+						scale: "Empress",
+						c: "#ffffff",
+						flecked: ["#BB29BB"],
+						colorName: "White, flecked purple, like mother-of-pearl"
+					}
+				],
+				scorpio: [
+					{
+						scale: "King",
+						c: "#00958d",
+						colorName: "Green blue"
+					},
+					{
+						scale: "Queen",
+						c: "#9D7446",
+						colorName: "Dull brown"
+					},
+					{
+						scale: "Emperor",
+						c: "#211307",
+						colorName: "Very dark brown"
+					},
+					{
+						scale: "Empress",
+						c: "#1c131b",
+						colorName: "Livid indigo brown (like a black beetle)"
+					}
+				],
+				sagittarius: [
+					{
+						scale: "King",
+						c: "#0085ca",
+						colorName: "Blue"
+					},
+					{
+						scale: "Queen",
+						c: "#FEDD00",
+						colorName: "Yellow"
+					},
+					{
+						scale: "Emperor",
+						c: "#00A550",
+						colorName: "Green"
+					},
+					{
+						scale: "Empress",
+						c: "#003a80",
+						colorName: "Dark vivid blue"
+					}
+				],
+				capricorn: [
+					{
+						scale: "King",
+						c: "#001489",
+						colorName: "Indigo"
+					},
+					{
+						scale: "Queen",
+						c: "#000000",
+						colorName: "Black"
+					},
+					{
+						scale: "Emperor",
+						c: "#000a44",
+						colorName: "Blue black"
+					},
+					{
+						scale: "Empress",
+						c: "#28292b",
+						colorName: "Cold dark grey, approaching black"
+					}
+				],
+				mars: [
+					{
+						scale: "King",
+						c: "#ed2000",
+						colorName: "Scarlet"
+					},
+					{
+						scale: "Queen",
+						c: "#ed1c24",
+						colorName: "Red"
+					},
+					{
+						scale: "Emperor",
+						c: "#c80815",
+						colorName: "Venetian red"
+					},
+					{
+						scale: "Empress",
+						c: "#ff0000",
+						rayed: "#4D91C6",
+						colorName: "Bright red, rayed azure or emerald"
+					}
+				],
+				aquarius: [
+					{
+						scale: "King",
+						c: "#440099",
+						colorName: "Violet"
+					},
+					{
+						scale: "Queen",
+						c: "#8ABAD3",
+						colorName: "Sky blue"
+					},
+					{
+						scale: "Emperor",
+						c: "#ad78bd",
+						colorName: "Blueish mauve"
+					},
+					{
+						scale: "Empress",
+						c: "#ffe8ff",
+						colorName: "White tinged purple"
+					}
+				],
+				pisces: [
+					{
+						scale: "King",
+						c: "#AE0E36",
+						colorName: "Crimson (ultra violet)"
+					},
+					{
+						scale: "Queen",
+						c: "#D8B998",
+						flecked: ["#F2F2F2"],
+						colorName: "Buff, flecked silver-white"
+					},
+					{
+						scale: "Emperor",
+						c: "#C08A80",
+						colorName: "Light translucent pinkish brown"
+					},
+					{
+						scale: "Empress",
+						c: "#76826a",
+						colorName: "Stone color"
+					}
+				],
+				sol: [
+					{
+						scale: "King",
+						c: "#FF6D00",
+						colorName: "Orange"
+					},
+					{
+						scale: "Queen",
+						c: "#FEDD00",
+						colorName: "Gold yellow"
+					},
+					{
+						scale: "Emperor",
+						c: "#FFA500",
+						colorName: "Rich amber"
+					},
+					{
+						scale: "Empress",
+						c: "#ffb734",
+						rayed: "#ed1c24",
+						colorName: "Amber, rayed red"
+					}
+				],
+				fire: [
+					{
+						scale: "King",
+						c: "#ff2a00",
+						colorName: "Glowing orange scarlet"
+					},
+					{
+						scale: "Queen",
+						c: "#D9381E",
+						colorName: "Vermilion"
+					},
+					{
+						scale: "Emperor",
+						c: "#ed2000",
+						flecked: ["#FEDD00"],
+						colorName: "Scarlet, flecked gold"
+					},
+					{
+						scale: "Empress",
+						c: "#D9381E",
+						flecked: ["#AE0E36", "#00A550"],
+						colorName: "Vermilion, flecked crimson and emerald"
+					}
+				],
+				saturn: [
+					{
+						scale: "King",
+						c: "#001489",
+						colorName: "Indigo"
+					},
+					{
+						scale: "Queen",
+						c: "#000000",
+						colorName: "Black"
+					},
+					{
+						scale: "Emperor",
+						c: "#000a44",
+						colorName: "Blue black"
+					},
+					{
+						scale: "Empress",
+						c: "#000000",
+						rayed: "#0085ca",
+						colorName: "Black, rayed blue"
+					}
+				],
+				spirit: [
+					{
+						scale: "King",
+						merging: ["#FFF", "#808080"],
+						colorName: "White merging into grey"
+					},
+					{
+						scale: "Queen",
+						c: "#220022",
+						colorName: "Deep purple, nearly black"
+					},
+					{
+						scale: "Emperor",
+						prismatic: ["#ed1c24","#FF6D00", "#FEDD00", "#00A550", "#0085ca", "#001489", "#440099"],
+						colorName: "The 7 prismatic colours, the violet being outside" 
+					},
+					{
+						scale: "Empress",
+						prismatic: ["#ffffff","#ed1c24", "#FEDD00", "#0085ca", "#000000"],
+						colorName: "White, red, yellow, blue, black (the latter outside)" 
+					}
+				],
+				earth: [
+					{
+						scale: "King",
+						quartered: ["#dedb2c", "#8d8800", "#731817", "#000000"],
+						colorName: "Citrine, olive, russet, and black" 
+					},
+					{
+						scale: "Queen",
+						c: "#ffb734",
+						colorName: "Amber"
+					},
+					{
+						scale: "Emperor",
+						c: "#38200a",
+						colorName: "Dark brown"
+					},
+					{
+						scale: "Empress",
+						c: "#000000",
+						flecked: ["#FEDD00"],
+						colorName: "Black, flecked yellow"
+					}
+				]
+		};
